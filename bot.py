d0ef3e857db382890a402cadadce9c6542788b11
Pavel-Bylkov <oldman.bpv@gmail.com>
2023-03-15 18:11:54 +0700

    add func to bot menu

--- bot.py
+++ bot.py
@@ -1,31 +1,26 @@
 # pip install pyTelegramBotAPI
-import socket
+from yaml import safe_load, YAMLError, safe_dump
+
+from create_config import CONFIG_FILE, LEAGUES_FILE
+
 
 import telebot
 from telebot import types
 
 from env_secret import TOKEN, CHAT_ID
 
-
 # Создаем экземпляр бота
 bot = telebot.TeleBot(TOKEN)
 
 active_users = {}
 
-def gen_markup(list_btns):
+
+def gen_markup_menu():
     """Создает ряды кнопок по 4 в ряд"""
-    n = len(list_btns)
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    if n > 4:
-        markup.row_width = n // 2
-        list_b = [types.KeyboardButton(btn) for btn in list_btns]
-        half_list_b = list_b[:len(list_b) // 2 + 1]
-        markup.add(*half_list_b)
-        markup.add(*list_b[len(list_b) // 2 + 1:])
-    else:
-        markup.row_width = n
-        list_b = [types.KeyboardButton(btn) for btn in list_btns]
-        markup.add(*list_b)
+    list_btns = ["Статус", "Подписчики", "Рестарт", "Настройки"]
+    list_b = [types.KeyboardButton(btn) for btn in list_btns]
+    markup.add(*list_b)
     return markup
 
 
@@ -34,9 +29,164 @@ def gen_markup(list_btns):
 def start(m, res=False):
     print(m.chat.id, m.text)
     if str(m.chat.id) in CHAT_ID:
-        bot.send_message(m.chat.id, 'Помощник готов к работе))')
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        list_btns = ["Статус", "Подписчики", "Рестарт", "Настройки"]
+        list_b = [types.KeyboardButton(btn) for btn in list_btns]
+        markup.add(*list_b)
+        bot.send_message(m.chat.id, 'Помощник готов к работе))', reply_markup=markup)
+        bot.register_next_step_handler(m, main_menu_choices)
     else:
         bot.send_message(m.chat.id, "Доступ к данному бот-сервису запрещен!")
+
+
+def main_menu_choices(m):
+    query = {"статус": get_status,
+             "подписчики": subscribes,
+             "рестарт": restart_pars,
+             "настройки": settings}
+    try:
+        query[m.text.lower()](m)
+    except Exception as e:
+        print(e)
+        bot.send_message(m.chat.id, 'Неверный выбор, попробуй еще раз')
+        bot.register_next_step_handler(m, main_menu_choices)
+
+
+def settings(m):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    list_btns = ["Список лиг", "Время матча", "Владение", "Удары", "Назад"]
+    list_b = [types.KeyboardButton(btn) for btn in list_btns]
+    markup.add(*list_b)
+    bot.send_message(m.chat.id, 'Какие настройки интересуют?', reply_markup=markup)
+    bot.register_next_step_handler(m, settings_actions)
+
+
+def settings_actions(m):
+    query = {"список лиг": league_menu,
+             "время матча": time_event,
+             "владение": passion,
+             "удары": shot_menu,
+             "назад": start}
+    try:
+        query[m.text.lower()](m)
+    except Exception as e:
+        print(e)
+        bot.send_message(m.chat.id, 'Неверный выбор, попробуй еще раз')
+        bot.register_next_step_handler(m, settings_actions)
+
+
+def league_menu(m):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    list_btns = ["Добавить", "Удалить", "Назад"]
+    list_b = [types.KeyboardButton(btn) for btn in list_btns]
+    markup.add(*list_b)
+    try:
+        with open(LEAGUES_FILE, 'r') as f:
+            leagues = f.read()
+    except Exception as e:
+        leagues = f"Ошибка чтения списка лиг - {e}"
+    bot.send_message(m.chat.id, leagues, reply_markup=markup)
+    bot.register_next_step_handler(m, league_action)
+
+def league_action(m):
+    query = {"добавить": input_league,
+             "удалить": choice_league,
+             "назад": settings}
+    try:
+        query[m.text.lower()](m)
+    except Exception as e:
+        print(e)
+        bot.send_message(m.chat.id, 'Неверный выбор, попробуй еще раз')
+        bot.register_next_step_handler(m, league_action)
+
+def input_league(m):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    list_btns = ["Назад"]
+    list_b = [types.KeyboardButton(btn) for btn in list_btns]
+    markup.add(*list_b)
+    bot.send_message(m.chat.id, "Напиши название лиги в формате - Страна: Лига", reply_markup=markup)
+    bot.register_next_step_handler(m, add_league)
+
+def add_league(m):
+    if m.text.lower() == "назад":
+        bot.send_message(m.chat.id, "Возврат")
+    else:
+        try:
+            with open(LEAGUES_FILE, 'r') as f:
+                leagues = f.read().split("\n")
+            leagues.append(m.text)
+            leagues.sort()
+            with open(LEAGUES_FILE, "w") as f:
+                f.write("\n".join(leagues))
+            bot.send_message(m.chat.id, m.text + " добавлена")
+        except Exception as e:
+            print(f"Ошибка чтения списка лиг - {e}")
+            bot.send_message(m.chat.id, f"{m.text} не добавлена, ошибка {e}")
+    bot.register_next_step_handler(m, league_menu)
+
+
+def choice_league(m):
+    msg = "Укажите номер строки списка лиг для удаления или нажмите Назад:"
+    bot.send_message(m.chat.id, msg)
+    try:
+        with open(LEAGUES_FILE, 'r') as f:
+            leagues = f.read().split("\n")
+        leagues.sort()
+        msg = "\n".join([f"{i} - {league}" for i, league in enumerate(leagues, 1)])
+        bot.send_message(m.chat.id, msg)
+    except Exception as e:
+        print(f"Ошибка чтения списка лиг - {e}")
+        bot.send_message(m.chat.id, f"Ошибка {e}")
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    list_btns = ["Назад"]
+    list_b = [types.KeyboardButton(btn) for btn in list_btns]
+    markup.add(*list_b)
+    bot.send_message(m.chat.id, "Напиши название лиги в формате - Страна: Лига", reply_markup=markup)
+    bot.register_next_step_handler(m, del_league)
+
+
+def del_league(m):
+    if m.text.lower() == "назад":
+        bot.send_message(m.chat.id, "Возврат")
+    elif not str(m.text).isdigit():
+        bot.send_message(m.chat.id, f"{m.text} - не верный формат ввода")
+    else:
+        try:
+            with open(LEAGUES_FILE, 'r') as f:
+                leagues = f.read().split("\n")
+            leagues.sort()
+            league = leagues.pop(int(m.text) - 1)
+            with open(LEAGUES_FILE, "w") as f:
+                f.write("\n".join(leagues))
+            bot.send_message(m.chat.id, league + " удалена")
+        except Exception as e:
+            print(f"Ошибка чтения списка лиг - {e}")
+            bot.send_message(m.chat.id, f"{m.text} не удалена, ошибка {e}")
+    bot.register_next_step_handler(m, league_menu)
+
+
+def shot_menu(m):
+    bot.send_message(m.chat.id, 'Функция в разработке...')
+
+
+def time_event(m):
+    bot.send_message(m.chat.id, 'Функция в разработке...')
+
+
+def passion(m):
+    bot.send_message(m.chat.id, 'Функция в разработке...')
+
+
+def get_status(m):
+    bot.send_message(m.chat.id, 'Функция в разработке...')
+
+
+def restart_pars(m):
+    bot.send_message(m.chat.id, 'Функция в разработке...')
+
+
+def subscribes(m):
+    bot.send_message(m.chat.id, 'Функция в разработке...')
 
 
 # Получение сообщений от юзера
